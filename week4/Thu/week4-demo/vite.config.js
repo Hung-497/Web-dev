852e123ef38bc4cf47ce17f63a6d401a7f87058b
Hung-497 <giahung092007@gmail.com>
2026-09-10 09:18:19 +0300

    add week 4 thursday

---
+++ week4/Thu/week4-demo/vite.config.js
@@ -0,0 +1,8 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base : "/Web-dev/tree/main/week4/Wed/week4-demo",
+})
